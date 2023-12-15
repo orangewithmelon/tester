e55c9795ad8ba5ed5d4a48a132c6c5ab679c53de
BT_2N2_24 <sample@bths.edu>
2023-12-15 13:51:31 -0500

    hi

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("watermelon");
-        System.out.println("ohelara ");
+        System.out.println("jnefsef");
         System.out.println("hi this is me");
 
         int[] arr = {1, 2, 3, 4, 5, 6};
